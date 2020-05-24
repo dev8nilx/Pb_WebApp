d46f6d07f7a29de06d63a47de0683fa6be47d82d
dev8nilx <dev.nilx@gmail.com>
2020-05-24 20:42:14 +0300

    v 1.0.0 Modif. Shell Script

--- L2_ShellScript/script.sh
+++ L2_ShellScript/script.sh
@@ -1,1 +1,10 @@
 #1/bin/bash
+echo "Enter file name:"
+read FileName
+touch $FileName.txt
+
+echo "Enter directory name:"
+read DirName
+mkdir $DirName
+
+mv $FileName.txt $DirName/"$FileName ($(date +%d-%m-%Y\ %H:%M:%S))".txt
